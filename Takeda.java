60ac525b9afd91fbecf3cef893b1faae975391b9
kurea <passionately.66.meltdown.4949@gmail.com>
2020-11-13 21:10:08 +0900

    Takeda更新

--- Takeda.java
+++ Takeda.java
@@ -3,8 +3,8 @@ public class Takeda{
 	public static void main(String[] args){
 		int[] numsA=new int[10];
 		int[] numsB=new int[10];
+		int[] index=new int[10];
 		int count=0;
-		int[] indexs=new int
 		for(int i=0;i<numsA.length;i++){
 			numsA[i]=new Random().nextInt(5)+1;
 			numsB[i]=new Random().nextInt(5)+1;
@@ -16,14 +16,40 @@ public class Takeda{
 				if(count==0){
 					System.out.printf("[%d]",i);
 					count++;
+				}else{
+					System.out.printf(",[%d]",i);
+					count++;
 				}
-				System.out.printf(",[%d]",i);
 			}
 		}
 		if(count==0){
 			System.out.println("一致しているindexはありませんでした。");
 		}else{
-			System.out.printf("の合計%d個が一致しています。",count);
+			System.out.printf("の合計%d個が一致しています。%n",count);
+		}
+		int[] counts=new int[count];
+		int comp=0;
+		for(int i=0;i<numsA.length;i++){
+			if(numsA[i]==numsB[i]){
+					counts[0+comp]=numsA[i];
+					index[comp]=i;
+					comp++;
+			}
+		}
+		for(int i=0;i<counts.length;i++){
+			for(int j=i+1;j<counts.length;j++){
+				if(counts[i] > counts[j]){
+					int temp=counts[i];
+					counts[i]=counts[j];
+					counts[j]=temp;
+					temp=index[i];
+					index[i]=index[j];
+					index[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<counts.length;i++){
+			System.out.printf("index[%d]:%d%n",index[i],counts[i]);
 		}
 	}
 }
